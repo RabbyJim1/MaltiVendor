f97b09849657be0b3de722f7f8afe81e0eb41fbc
Md. Golam Rabby Jim <jim15-5551@diu.bd>
2018-12-14 01:17:32 +0600

    WebView done!

--- app/src/main/java/com/example/rabby/maltivendor/MainActivity.java
+++ app/src/main/java/com/example/rabby/maltivendor/MainActivity.java
@@ -1,13 +1,57 @@
 package com.example.rabby.maltivendor;
 
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private ProgressBar loadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.webViewID);
+        loadingBar = (ProgressBar) findViewById(R.id.prg);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                loadingBar.setVisibility(View.VISIBLE);
+                setTitle("Loading...");
+
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                loadingBar.setVisibility(View.GONE);
+                setTitle(view.getTitle());
+            }
+        });
+
+        webView.loadUrl("http://multivendor.devsdiv.com/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }else{
+            super.onBackPressed();
+        }
+
     }
 }
